cffd23d4a79f6a6210596c68effe3008fa09847c
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-07-22 15:55:58 -0400

    Initial commit

---
+++ Books App/Books_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Books_AppApp.swift
+//  Books App
+//
+//  Created by Joe Santhosh on 2021-07-22.
+//
+
+import SwiftUI
+
+@main
+struct Books_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Books App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Books App
+//
+//  Created by Joe Santhosh on 2021-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
