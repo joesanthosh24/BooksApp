04b61de0b6d8a8d3ad1d766a488d5d210aa5d429
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-07-24 17:25:33 -0400

    moved Book struct to its own file in Models folder. Created a BookView to display Book Image, title and author. Added some images in xcassets folder

--- Books App/ContentView.swift
+++ Books App/ContentView.swift
@@ -9,25 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     @State var searchContent: String = ""
+    
     var books: [Book] = [
-        Book(title: "Huckleberry Fin"),
-        Book(title: "Harry Potter and the Sorceror's Stone")
+        Book(title: "Huckleberry Fin", author: "Mark Twain", image: "huck_fin"),
+        Book(title: "Harry Potter and the Sorceror's Stone", author: "J.K. Rowling", image: "harry_sorc"),
+        Book(title: "The Lightning Thief", author: "Rick Riordian", image: "lightning"),
+        Book(title: "The Mamba Mentality, How I Play", author: "Kobe Bryant", image: "mamba"),
+        Book(title: "Ikigai: Japanese Secret to Long and Happy Life", author: "Hector Garcia and Francesc Miralles", image: "ikigai"),
+        Book(title: "The Wizard of Oz", author: "L. Frank Baum", image: "wizard_oz"),
+        Book(title: "Harry Potter and the Sorceror's Stone", author: "J.K. Rowling", image: "harry_sorc")
     ]
     
     var body: some View {
         NavigationView {
-//            List(books) { book in
-//                Text(book.title)
-//            }
-            List {
-                Text(books[0].title)
-                Text(books[1].title)
+            VStack {
+                HStack {
+                    TextField("Search For A Book", text: $searchContent)
+                        .frame(height: 1, alignment: .center)
+                        .padding()
+                        .background(Color(.systemGray4))
+                        .cornerRadius(5)
+                }
+                Section {
+                    ScrollView {
+                        ForEach(0..<books.count) {
+                            BookView(book: books[$0])
+                        }
+                    }
+                }
+                Spacer()
             }
-            .foregroundColor(.black)
+            .padding()
             .navigationBarTitle("Books")
         }
-        .foregroundColor(.white)
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
@@ -37,6 +51,31 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Book {
-    var title: String
+struct BookView: View {
+    let book: Book
+    
+    var body: some View {
+        HStack {
+            Image("\(book.image)")
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+            VStack {
+                HStack {
+                    Text((book.title))
+                        .foregroundColor(Color(.label))
+                        .fontWeight(.medium)
+                    Spacer()
+                }
+                HStack {
+                    Text("by \(book.author)")
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                Spacer()
+            }
+            .frame(alignment: .leading)
+            Spacer()
+        }
+        .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 10))
+    }
 }
---
+++ Books App/Models/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  Books App
+//
+//  Created by Joe Santhosh on 2021-07-24.
+//
+
+import Foundation
+
+struct Book {
+    var title: String
+    var author: String
+    var image: String
+}
