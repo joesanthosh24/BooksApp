06dd3976ed09a4b47c7c5d44440f6edfa94d41e1
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-08-11 10:24:49 -0400

    Got rid of text field

--- Books App/ContentView.swift
+++ Books App/ContentView.swift
@@ -8,32 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var searchContent: String = ""
+    @State private var searchContent: String = ""
     
-    var books: [Book] = [
-        Book(title: "Huckleberry Fin", author: "Mark Twain", image: "huck_fin", stars: 5),
-        Book(title: "Harry Potter and the Sorceror's Stone", author: "J.K. Rowling", image: "harry_sorc", stars: 5),
-        Book(title: "The Lightning Thief", author: "Rick Riordian", image: "lightning", stars: 4),
-        Book(title: "The Mamba Mentality, How I Play", author: "Kobe Bryant", image: "mamba", stars: 5),
-        Book(title: "Ikigai: Japanese Secret to Long and Happy Life", author: "Hector Garcia and Francesc Miralles", image: "ikigai", stars: 3),
-        Book(title: "The Wizard of Oz", author: "L. Frank Baum", image: "wizard_oz", stars: 4)
-    ]
+    @State private var books: [Book] = bookData
     
     var body: some View {
         NavigationView {
             VStack {
-                HStack {
-                    TextField("Search For A Book", text: $searchContent)
-                        .frame(height: 1, alignment: .center)
-                        .padding()
-                        .background(Color(.systemGray4))
-                        .cornerRadius(5)
-                    Button(action: {
-                        
-                    }, label: {
-                        Image(systemName: "magnifyingglass")
-                    })
-                }
                 Section {
                     ScrollView {
                         ForEach(0..<books.count) { num in
@@ -51,7 +32,7 @@ struct ContentView: View {
             .navigationBarTitle("Books")
             .edgesIgnoringSafeArea(.bottom)
         }
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
--- Books App/Models/Book.swift
+++ Books App/Models/Book.swift
@@ -13,3 +13,12 @@ struct Book {
     var image: String
     var stars: Int
 }
+
+var bookData: [Book] = [
+    Book(title: "Huckleberry Fin", author: "Mark Twain", image: "huck_fin", stars: 5),
+    Book(title: "Harry Potter and the Sorceror's Stone", author: "J.K. Rowling", image: "harry_sorc", stars: 5),
+    Book(title: "The Lightning Thief", author: "Rick Riordian", image: "lightning", stars: 4),
+    Book(title: "The Mamba Mentality, How I Play", author: "Kobe Bryant", image: "mamba", stars: 5),
+    Book(title: "Ikigai: Japanese Secret to Long and Happy Life", author: "Hector Garcia and Francesc Miralles", image: "ikigai", stars: 3),
+    Book(title: "The Wizard of Oz", author: "L. Frank Baum", image: "wizard_oz", stars: 4)
+]
